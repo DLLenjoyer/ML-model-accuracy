b8b3c49de1645ef86f42ab68838294d4f12c1bf4
0x2170 <151378415+DLLenjoyer@users.noreply.github.com>
2024-07-27 19:09:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models, datasets
 
-# Загрузка данных MNIST
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
 
-# Подготовка данных
 train_images = train_images.reshape((60000, 28, 28, 1))
 train_images = train_images.astype('float32') / 255
 
 test_images = test_images.reshape((10000, 28, 28, 1))
 test_images = test_images.astype('float32') / 255
 
-# Определение модели CNN
+# CNN
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
